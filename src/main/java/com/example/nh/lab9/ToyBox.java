973ad21902c1c4aab0c45de09bcd95d4e37d3240
jeffersonhd <jefferson123hd@gmail.com>
2018-11-30 20:22:44 -0500

    example

---
+++ src/main/java/com/example/nh/lab9/ToyBox.java
@@ -0,0 +1,24 @@
+package com.example.nh.lab9;
+
+public class ToyBox {
+    public static String[] getToyNames(){
+        return new String[]{
+                "Auto",
+                "Bicicleta",
+                "Avion",
+                "carro",
+                "camion",
+                "bus",
+                "aeroplano",
+                "avioneta",
+                "barco",
+                "planeador",
+                "Avion",
+                "Avion",
+                "Avion",
+                "Avion"
+
+        };
+    }
+
+}
